e30e986260e30c4e6dd79c6cb8d73c024ede4fba
Oksana <kroshkaksju@gmail.com>
2020-07-18 00:45:41 +0300

    spawn, dispawn monsters

--- Assets/Scripts/RoomMove.cs
+++ Assets/Scripts/RoomMove.cs
@@ -1,24 +1,23 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using Lean.Pool;
 using UnityEngine;
 
 public class RoomMove : MonoBehaviour
 {
-    [SerializeField] private Vector3 characterChange;
+    [SerializeField] private Vector3 characterChange; 
     [SerializeField] private Vector2 cameraChange;
-    [SerializeField] private Vector3 spownMonsterposition;
-    [SerializeField] private GameObject monsterPrefab;
+    public List<GameObject>  objectsToDestroy = new List<GameObject>();
+    public GameObject[] objectsToCreate;
+    public Vector3[] positionsToCreate;
+    [SerializeField] private RoomMove anotherSideOfTheDoor;
+    
+    private List<GameObject> instantiatedObjects = new List<GameObject>();
 
     private CameraController _cameraController;
-    private Monster _monster;
 
     private void Awake()
     {
         _cameraController =  Camera.main.GetComponent<CameraController>();
-    }
-
-    private void Start()
-    {
-        _monster = FindObjectOfType<Monster>();
     }
 
     public void OnTriggerEnter2D(Collider2D other)
@@ -28,10 +27,30 @@ public class RoomMove : MonoBehaviour
             _cameraController.minPosition += cameraChange;
             _cameraController.maxPosition += cameraChange;
             other.transform.position += characterChange;
-            if (_monster == null )
+            DestroyObjects(anotherSideOfTheDoor.instantiatedObjects);
+            DestroyObjects(objectsToDestroy);
+            CreateObjects();
+        }
+    }
+
+    private void DestroyObjects(List<GameObject> list)
+    {
+        foreach (var obj in list)
+        {
+            if (obj == null)
             {
-                Instantiate(monsterPrefab, spownMonsterposition, transform.rotation);
+                continue;
             }
+            LeanPool.Despawn(obj);
+        }
+    }
+
+    private void CreateObjects()
+    {
+        for (int i = 0; i < objectsToCreate.Length; i++)
+        {
+            GameObject obj = LeanPool.Spawn(objectsToCreate[i], positionsToCreate[i], transform.rotation);
+            instantiatedObjects.Add(obj);
         }
     }
 }
