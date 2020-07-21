1a5b164bb7bde328d8acfc7cc36c294da59eb0e3
Oksana <kroshkaksju@gmail.com>
2020-07-21 22:14:21 +0300

    Create SceneLoader

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneLoader : MonoBehaviour
+{
+    [SerializeField] private float reloadLevelDelay;
+    #region Singletone
+
+    public static SceneLoader Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject); 
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+    #endregion
+    public  void RestartLevel(float delay)
+    {
+        StartCoroutine(RestartLevelCoroutine(delay));
+    }
+    
+    IEnumerator RestartLevelCoroutine(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        
+        int currentScene = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentScene);
+    }
+    
+    public void RestartLevel()
+    {
+        // получить индекс текущей сцены
+        int ccurentScene = SceneManager.GetActiveScene().buildIndex;
+        // перезагрузка текущей сцены
+        SceneManager.LoadScene(ccurentScene);
+    }
+    
+    public void LoadNextScene()
+    {
+        int activeSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(activeSceneIndex + 1, LoadSceneMode.Single);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
